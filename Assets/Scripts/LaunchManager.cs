2f4d9804e48439309020edde74de54b9ec1177ef
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-05-26 08:00:00 +0100

    Initial Commit

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    #endregion
+
+    #region Public Methods
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log( PhotonNetwork.NickName + " Connected to Photon servers");
+    }
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to internet");
+    }
+    #endregion
+}
